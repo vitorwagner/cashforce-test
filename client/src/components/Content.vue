d117ff52b98466833fe98f6eb3702e0dc0a351d1
Vitor Wagner <vitormwagner@gmail.com>
2024-08-01 22:54:06 -0300

    feat: Add Navbar, TableComponent, and Content components

---
+++ client/src/components/Content.vue
@@ -0,0 +1,8 @@
+<script setup>
+import TableComponent from './TableComponent.vue';
+</script>
+
+<template>
+  <h3>Content</h3>
+  <TableComponent />
+</template>
---
+++ client/src/components/TableComponent.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>Table</p>
+</template>
